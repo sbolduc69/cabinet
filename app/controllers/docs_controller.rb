59483e62c64056b72fc6dac765d39081ea4ac59c
Scott Bolduc <scott.bolduc@gmail.com>
2017-01-23 20:28:37 -0700

    edit and destroy docs

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,5 +1,6 @@
 class DocsController < ApplicationController
   before_action :find_doc, only: [:show, :edit, :update, :destroy]
+
   def index
     @docs = Doc.all.order("created_at DESC")
   end
@@ -25,9 +26,16 @@ class DocsController < ApplicationController
   end
 
   def update
+    if @doc.update(doc_params)
+      redirect_to @doc
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @doc.destroy
+    redirect_to docs_path
   end
 
   private
